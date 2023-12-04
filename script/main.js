a965b90109a6028448b24a727fc0902ab70bed05
Saskreet Bhattarai <115059716+saskreet@users.noreply.github.com>
2023-12-04 22:36:38 +0545

    Update main.js

--- script/main.js
+++ script/main.js
@@ -97,9 +97,72 @@ const animationTimeline = () => {
         ease: Elastic.easeOut.config(1, 0.5)
       },
       0.1
-    );
+    )
+  .staggerFromTo(
+".wish-hbd span",
+0.7,
+{
+scale: 1.4,
+rotationY: 150
+},
+{
+scale: 1,
+rotationY: 0,
+color: "#ff69b4",
+ease: Expo.easeOut
+},
+0.1,
+"party"
+)
+.from(
+".wish h5",
+0.5,
+{
+opacity: 0,
+y: 10,
+skewX: "-15deg"
+},
+"party"
+)
+.staggerTo(
+".eight svg",
+1.5,
+{
+visibility: "visible",
+opacity: 0,
+scale: 80,
+repeat: 3,
+repeatDelay: 1.4
+},
+0.3
+)
+.to(".six", 0.5, {
+opacity: 0,
+y: 30,
+zIndex: "-1"
+})
+.staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
+.to(
+".last-smile",
+0.5,
+{
+rotation: 90
+},
+"+=1"
+);
 
-  // ... (remaining existing code)
+// tl.seek("currentStep");
+// tl.timeScale(2);
+
+// Restart Animation on click
+const replyBtn = document.getElementById("replay");
+replyBtn.addEventListener("click", () => {
+tl.restart();
+});
+};
+
+// Run fetch and animation in sequence
+fetchData();
 };
 
 // Run fetch and animation in sequence
